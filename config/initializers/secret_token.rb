a5b5e7ce5905f03ff6b6fdc7bd9f262ee75dc9c2
kdurr <kristina.l.durr@gmail.com>
2013-09-26 12:14:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealEstate::Application.config.secret_key_base = '6cc8a43d132a8492db2e8140e1a7b7fa2a576c20a6f2cf2445a554db2875f210d682138373cac4acc5069d62f888469e412f11c3afdfccd2ee6ee3f889f9f2d8'
